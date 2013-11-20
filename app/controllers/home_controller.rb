6cac949a0bcf37d3f716e30cd752778463dd61f9
sickle <splevako1@gmail.com>
2013-11-20 22:03:49 +0200

    added more photos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+include Instagram::Client::Tags 
+include Instagram::Client::Media
   def index
     @instagram = Instagram.user_recent_media("686493004", {:count => 1})
+    #TODO: need to find out way to find by specific tag
+    #@tags = Instagram.tag("sun")
+    @tags = Instagram.media_popular
   end
 end
