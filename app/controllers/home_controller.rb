0512e7672b1a0132f9fcfcfe50e8df15793983f7
sickle <splevako1@gmail.com>
2013-11-20 22:26:53 +0200

    minor changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
-include Instagram::Client::Tags 
-include Instagram::Client::Media
+#include Instagram::Client::Tags 
+#include Instagram::Client::Media
   def index
     @instagram = Instagram.user_recent_media("686493004", {:count => 1})
     #TODO: need to find out way to find by specific tag
