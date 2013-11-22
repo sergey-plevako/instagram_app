01b07a5ed67d66932eea1716ee484ccac55dd1ae
Sergey Plevako <splevako1@gmail.com>
2013-11-22 10:23:28 +0200

    Added cap recipe (WIP)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,18 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'InstagramApp'
+set :repo_url, 'git@github.com:sergey-plevako/instagram_app.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+ set :deploy_to, '/opt/instagram_app'
+ set :scm, :git
+ set :branch, "master"
 
 # set :format, :pretty
 # set :log_level, :debug
 # set :pty, true
+set :use_sudo, false
+set :deploy_via, :copy	
+set :ssh_options, { :forward_agent => true, :port => 4321 }
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
